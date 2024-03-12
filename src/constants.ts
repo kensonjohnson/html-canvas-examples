9dbfd7dc6f002ef05148125348ab0692f9697186
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-03-11 19:56:01 -0500

    Add pan

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
 export const headerLinks = [
   { name: "Home", path: "/" },
   { name: "Zoom", path: "/zoom/" },
+  { name: "Pan", path: "/pan/" },
+  // { name: "Pan & Zoom", path: "/pan-zoom/" },
 ];
---
+++ src/pan/pan.ts
@@ -0,0 +1,21 @@
+// Import custom web components
+import "../classes/Header.ts";
+
+import { CanvasPan } from "../classes/CanvasPan.ts";
+
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+
+const canvasPan = new CanvasPan(canvas);
+
+const buttonContainer = document.createElement("div");
+buttonContainer.classList.add("controls");
+
+const resetButton = document.createElement("button");
+resetButton.textContent = "Reset Position";
+resetButton.onclick = () => {
+  canvasPan.resetPosition();
+};
+
+buttonContainer.appendChild(resetButton);
+document.body.appendChild(buttonContainer);
